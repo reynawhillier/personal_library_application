ae0ac84cd4931adbcf9f021d9906a327cf720b01
reynawhillier <reyna@whillier.com>
2026-08-14 23:45:21 -0400

    Task 3: Implemented User Table
    - created role enum for hardcoded ids
    - created role migration, model, and seeder
    - added role foreign key to user table
    - added default seeder with role types

--- database/schema.ts
+++ database/schema.ts
@@ -7,6 +7,19 @@
 import { BaseModel, column } from '@adonisjs/lucid/orm'
 import { DateTime } from 'luxon'
 
+export class RoleSchema extends BaseModel {
+  static $columns = ['createdAt', 'id', 'name', 'updatedAt'] as const
+  $columns = RoleSchema.$columns
+  @column.dateTime({ autoCreate: true })
+  declare createdAt: DateTime | null
+  @column({ isPrimary: true })
+  declare id: number
+  @column()
+  declare name: string
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  declare updatedAt: DateTime | null
+}
+
 export class UserSchema extends BaseModel {
   static $columns = ['createdAt', 'email', 'fullName', 'id', 'password', 'updatedAt'] as const
   $columns = UserSchema.$columns
